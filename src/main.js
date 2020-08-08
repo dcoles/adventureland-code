4e2a1400896551494c8fb97f50b4c5c2d708f21e
David Coles <coles.david@gmail.com>
2020-08-08 14:09:54 -0700

    Implement simple state machine for character actions

--- src/main.js
+++ src/main.js
@@ -15,8 +15,17 @@ import {
 const HEARTBEAT_INTERVAL_MS = 250;
 const TARGET_MAX_HP_RATIO = 10.00;
 const TARGET_MAX_ATTACK_RATIO = 0.80;
-const TARGET_MIN_XP_RATIO = 0.0025;
+const TARGET_MIN_XP_RATIO = 0.003;
 const CRITICAL_HP_RATIO = 0.10;
+
+const IDLE = 'Idle';
+const FIND_TARGET = 'Finding target';
+const ADVANCE = 'Advancing';
+const ATTACK = 'Attacking';
+const FLEE_TO_TOWN = 'Fleeing to Town';
+
+/** Current behaviour */
+let state = IDLE;
 
 /** What are valid monster types to attack? */
 function valid_monster_types() {
@@ -45,35 +54,76 @@ function pick_target() {
 	return target;
 }
 
+/** Calculate critical HP */
+function critical_hp() {
+	return CRITICAL_HP_RATIO * character.hp;
+}
+
 /** Regular heartbeat */
 function heartbeat() {
+	// Always do these actions
 	regen_hp_or_mp();
 	loot();
 
-	if (character.hp < CRITICAL_HP_RATIO * character.max_hp) {
-		set_message('Critical HP!')
-		use_skill('use_town');
-		return;
+	if (character.hp < critical_hp()) {
+		state = FLEE_TO_TOWN;
 	}
 
-	let target = pick_target();
-	if (!target) {
-		set_message('No Monsters');
-		return;
+	let target = get_target();
+	switch (state) {
+		case IDLE:
+			// fallthrough
+
+		case FIND_TARGET:
+			target = pick_target();
+			change_target(target);
+
+			if (!target) {
+				state = IDLE;
+				return;
+			}
+
+			// fallthrough
+
+		case ADVANCE:
+			if (!target) {
+				state = IDLE;
+				return;
+			}
+
+			if (!is_in_range(target)) {
+				move_towards(target);
+				return;
+			}
+
+			// fallthrough
+
+		case ATTACK:
+			if (is_on_cooldown('attack')) {
+				// Wait...
+				return;
+			}
+
+			attack(target);
+			break;
+
+		case FLEE_TO_TOWN:
+			use_skill('use_town');
+			break;
+	
+		default:
+			log('Unknown state: ' + state, 'red');
+			state = IDLE;
+			break;
 	}
 
-	change_target(target);
-	if (!is_in_range(target)) {
-		set_message('Advancing');
-		move_towards(target);
-	} else if (can_attack(target)) {
-		set_message('Attacking');
-		attack(target);
-	}
+	set_message(state);
 }
 
 /** Main function */
 function main() {
+	log('Starting...');
+
 	// Start heartbeat
 	setInterval(heartbeat, HEARTBEAT_INTERVAL_MS);
 }
