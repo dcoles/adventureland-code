d2cdd0f78c1cfd8d507b7e8c0a07ca8494265642
David Coles <coles.david@gmail.com>
2020-08-08 10:12:24 -0700

    loader: Enable strict mode
    This is to help catch many common coding bugs.

--- src/loader.js
+++ src/loader.js
@@ -1,10 +1,19 @@
 /**
-  * Fetch code from remote URL and run it.
+ * Fetch code from remote URL and run it.
  *
  * ⚠️ CAUTION -- Only use this with URLs you control and trust.
  * Untrusted code may hijack your account, use it to mine crypto currency,
  * steal personal data, cause your cat to run away and other nasty things.
  */
+
+ /**
+  * Enable strict mode to help catch more bugs.
+  *
+  * See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+  */
+ "use strict";
+
+/** URL to fetch code from */
 const URL = "http://127.0.0.1:5500/src/main.js";
 
 fetch(URL)
