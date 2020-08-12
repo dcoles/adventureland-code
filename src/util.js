e09effdd80ae62beb49b4566b7b2f0bf0f55861c
David Coles <coles.david@gmail.com>
2020-08-11 21:57:40 -0700

    Add ts-check for util module
    This required a small code change to make sure that we don't rely upon
    implicit conversion from `Date` to `number`.

--- src/util.js
+++ src/util.js
@@ -1,15 +1,26 @@
 // General utility functions
+// @ts-check
 
-/** Promise that resolves after timeout */
+/**
+ * Promise that resolves after timeout.
+ * 
+ * @param {number} ms Timeout in milliseconds.
+*/
 export function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-/** Sleep until specific `Date` */
+/**
+ * Sleep until specific date-time.
+ * 
+ * @param {Date} date Date-time to sleep until.
+*/
 export async function sleep_until(date) {
+	const ts = date.getTime();
+
 	let now = Date.now();
-	while (now <= date) {
-		await sleep(date - now);
+	while (now <= ts) {
+		await sleep(ts - now);
 		now = Date.now();
 	}
 }
