ee1ad74c1d12ae9dcb5af19c8fac07e827bfb061
David Coles <coles.david@gmail.com>
2020-08-22 09:23:57 -0700

    Add distance function to util.
    This is a simple distance function that calculates the distance between
    two points and nothing more.

--- src/util.js
+++ src/util.js
@@ -2,10 +2,10 @@
 // @ts-check
 
 /**
- * Promise that resolves after timeout.
+ * Sleep for a period of time.
  * 
- * @param {number} ms Timeout in milliseconds.
- * @returns {Promise}
+ * @param {number} ms Time in milliseconds.
+ * @returns {Promise} A promise that resolves after time period has elapsed.
 */
 export function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
@@ -24,4 +24,23 @@ export async function sleep_until(date) {
 		await sleep(ts - now);
 		now = Date.now();
 	}
+}
+
+/**
+ * Calculate the distance between two points (`x1`, `y1`) and (`x2`, `y2`).
+ *
+ * This differs from the built-in `distance` function that does something
+ * that is almost, but not quite, entirely unlike returning the distance
+ * between two points (it lacks any documentation).
+ *
+ * @see https://adventure.land/docs/code/functions/distance
+ *
+ * @param {number} x1 x-coordinate of first point.
+ * @param {number} y1 y-coordinate of first point.
+ * @param {number} x2 x-coordinate of second point.
+ * @param {number} y2 y-coordinate of second point.
+ * @returns {number} Distance in pixels.
+ */
+export function distance(x1, y1, x2, y2) {
+	return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 }
