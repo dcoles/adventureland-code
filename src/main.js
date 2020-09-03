94cd7a28a3342ff4f1b054418a4ddc5b0933f286
David Coles <coles.david@gmail.com>
2020-09-02 18:20:58 -0700

    Move stat_monitor into Widget module
    This module will hold general purpose UI widgets.

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ import * as Util from './util.js';
 import * as Character from './character.js';
 import * as BattleLog from './battlelog.js';
 import * as Command from './command.js';
+import * as Widgets from './widgets.js';
 
 // Your Character
 export const character = Character.get_character();
@@ -603,41 +604,6 @@ window.on_party_request = function(name) {
 	}
 }
 
-/**
- * Monitor a character stat and display the rate.
- *
- * Calculates an weighted moving average over `n` samples.
- *
- * @param {string} stat_name Stat name (e.g. 'xp').
- * @param {number} [t=1] Time interval (seconds).
- * @param {number} [window_duration=600] Sample window in seconds (default: 10m).
- */
-function stat_monitor(stat_name, t, window_duration) {
-	if (character.bot) {
-		return;
-	}
-
-	t = t || 1;
-	window_duration = window_duration || 600;
-	const n = Math.floor(window_duration / t);  // Number of samples in window
-	let last = character[stat_name];
-	let values = [];
-	window.setInterval(() => {
-		const current = character[stat_name];
-		const per_second = (current - last) / t;
-
-		// Weighted moving average over `n` samples
-		if (values.push(per_second) > n) {
-			values.shift();
-		}
-		const avg_per_hour = 3600 * values.reduce((a, b) => a + b) / n;
-
-		// Show at bottom of the UI
-		add_bottom_button(stat_name + '_rate', `${stat_name.toUpperCase()}/h: ${Math.round(avg_per_hour)}`);
-		last = current;
-	}, t * 1000);
-}
-
 /** Main function */
 async function main() {
 	Logging.info('== Starting CODE ==')
@@ -658,8 +624,8 @@ async function main() {
 	});
 
 	// Show XP/s and GOLD/s
-	stat_monitor('xp');
-	stat_monitor('gold')
+	Widgets.stat_monitor('xp');
+	Widgets.stat_monitor('gold')
 
 	// Map snippets
 	Adventure.map_snippet('G', 'Code.set_state("Return Home")');
