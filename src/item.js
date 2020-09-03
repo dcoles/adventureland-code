cbb1f1029c2342b118eeafc233c6017ded38685c
David Coles <coles.david@gmail.com>
2020-09-02 18:32:12 -0700

    Add functions for compounding and upgrading items

---
+++ src/item.js
@@ -0,0 +1,100 @@
+// Item upgrades and compounding
+// @ts-check
+import * as Adventure from './adventure.js';
+import * as Logging from './logging.js';
+
+/**
+ * Compound all of a certain type of item.
+ *
+ * @param {string} name Name of item (e.g. "hpamulet").
+ * @param {number} max_level Maximum level to compound to.
+ * @param {string} [scroll='cscroll0'] Combining scroll.
+ */
+export async function compound_all(name, max_level, scroll) {
+	scroll = scroll || 'cscroll0';
+
+	await Adventure.smart_move('compound');
+	for (let level=0; level<max_level; level++) {
+		const i_scrolls = indexed_items({name: scroll});
+		const i_items = indexed_items({name: name, level: level});
+
+		// Combine!
+		for (let i=0; i<i_items.length-2; i+=3) {
+			const i_scroll = i_scrolls[0];
+			if (!i_scroll || i_scroll[1].q < 1) {
+				// No more scrolls
+				Logging.warn(`Can't compound: No ${G.items[scroll].name}`);
+				return;
+			}
+
+			try {
+				Logging.info(`Compounding ${G.items[name].name} (${level} to ${level+1})`);
+				await window.compound(i_items[i][0], i_items[i+1][0], i_items[i+2][0], i_scroll[0]);
+			} catch (e) {
+				Logging.warn('Compounding failed', e.reason);
+			}
+		}
+	}
+}
+
+/**
+ * Upgrade all of a certain item.
+ *
+ * @param {string} name Name of item (e.g. "slimestaff").
+ * @param {number} max_level Maximum level to upgrade to.
+ * @param {string} [scroll='scroll0'] Upgrade scroll.
+ */
+export async function upgrade_all(name, max_level, scroll) {
+	scroll = scroll || 'scroll0';
+
+	await Adventure.smart_move('upgrade');
+	for (let level=0; level<max_level; level++) {
+		const i_scrolls = indexed_items({name: scroll});
+		const i_items = indexed_items({name: name, level: level});
+
+		// Upgrade!
+		for (let i=0; i<i_items.length; i++) {
+			const i_scroll = i_scrolls[0];
+			if (!i_scroll || i_scroll[1].q < 1) {
+				// No more scrolls
+				Logging.warn(`Can't upgrade: No ${G.items[scroll].name}`);
+				return;
+			}
+
+			try {
+				Logging.info(`Upgrading ${G.items[name].name} (${level} to ${level+1})`);
+				await window.upgrade(i_items[i][0], i_scroll[0]);
+			} catch (e) {
+				Logging.warn('Upgrading failed', e.reason);
+			}
+		}
+	}
+}
+
+/**
+ * Get indexed character items.
+ *
+ * @param {object} [filter] Filter the returned items.
+ * @param {string} [filter.name] Match item name.
+ * @param {number} [filter.level] Match item level.
+ * @returns {[number, object][]} Array of `[index, item]` tuples.
+*/
+function indexed_items(filter) {
+	filter = filter || {};
+	return character.items.map((item, index) => [index, item]).filter(([_, item]) => {
+		if (!item) {
+			// Empty slot
+			return false;
+		}
+
+		if (filter.name && item.name !== filter.name) {
+			return false;
+		}
+
+		if (Number.isInteger(filter.level) && item.level !== filter.level) {
+			return false;
+		}
+
+		return true;
+	});
+}
--- src/main.js
+++ src/main.js
@@ -16,6 +16,7 @@ import * as Character from './character.js';
 import * as BattleLog from './battlelog.js';
 import * as Command from './command.js';
 import * as Widgets from './widgets.js';
+import * as Item from './item.js';
 
 // Your Character
 export const character = Character.get_character();
@@ -578,6 +579,50 @@ class Brain {
 }
 
 /**
+ * Attempt to compound items.
+ *
+ * @param {string} name Item name (e.g. "hpamulet")
+ * @param {number} [max_level=4] Maximum level to upgrade to.
+ * @param {string} [scroll='cscroll0'] Combining scroll.
+ */
+export function compound_items(name, max_level, scroll) {
+	max_level = max_level || 2;
+	if (!G.items[name]) {
+		Logging.error(`Unknown item: ${name}`);
+		return;
+	}
+
+	set_message('Compounding', 'dodgerblue');
+	Logging.info(`Compounding all ${G.items[name].name} to level ${max_level}`);
+
+	Item.compound_all(name, max_level, scroll)
+	.then(() => { Logging.info('Finished compounding items'); set_message('Done') })
+	.catch((e) => { Logging.warn('Compounding items failed', e.reason); set_message('Failed', 'orange') });
+}
+
+/**
+ * Attempt to upgrade items.
+ *
+ * @param {string} name Item name (e.g. "slimestaff")
+ * @param {number} [max_level=4] Maximum level to upgrade to.
+ * @param {string} [scroll='scroll0'] Upgrade scroll.
+ */
+export function upgrade_items(name, max_level, scroll) {
+	max_level = max_level || 4;
+	if (!G.items[name]) {
+		Logging.error(`Unknown item: ${name}`);
+		return;
+	}
+
+	set_message('Upgrading', 'firebrick');
+	Logging.info(`Upgrading all ${G.items[name].name} to level ${max_level}`);
+
+	Item.upgrade_all(name, max_level, scroll)
+	.then(() => { Logging.info('Finished upgrading items'); set_message('Done') })
+	.catch((e) => { Logging.warn('Upgrading items failed', e.reason); set_message('Failed', 'orange') });
+}
+
+/**
  * Called when invited to join another character's party.
  *
  * @param {string} name Name of the character who sent the invitation.
@@ -635,6 +680,8 @@ async function main() {
 	Adventure.map_snippet('M', 'Code.set_target()');
 
 	// Map /commands
+	Command.register('compound', compound_items, ['item'], ['max_level:int', 'scroll']);
+	Command.register('upgrade', upgrade_items, ['item'], ['max_level:int', 'scroll']);
 	Command.register('go', Adventure.smart_move, ['location']);
 
 	// Start running!
