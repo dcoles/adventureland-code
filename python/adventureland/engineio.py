b04990bc5635ca50525fa0b33ef90c01b3ba882c
David Coles <coles.david@gmail.com>
2020-10-04 17:24:25 -0700

    Add simple Python Socket.IO client based on aiohttp

---
+++ python/adventureland/engineio.py
@@ -0,0 +1,229 @@
+import asyncio
+import contextlib
+import json
+import logging
+import time
+from dataclasses import dataclass
+from enum import Enum
+from urllib.parse import urljoin, urlencode
+from typing import *
+
+import aiohttp
+
+SECOND_MS = 1000
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class EngineIOPacket:
+    """
+    See https://github.com/socketio/engine.io-protocol/tree/v3.
+    """
+    class Type(Enum):
+        OPEN = 0
+        CLOSE = 1
+        PING = 2
+        PONG = 3
+        MESSAGE = 4
+        UPGRADE = 5
+        NOOP = 6
+
+    type: Type
+    data: str = ''
+
+    @classmethod
+    def parse(cls, data: str):
+        return cls(EngineIOPacket.Type(int(data[0])), data[1:])
+
+    def encode(self):
+        return '%d%s' % (self.type.value, self.data)
+
+    def __str__(self):
+        return f'{self.type.name} {self.data}' if self.data else self.type.name
+
+
+class EngineIO:
+    """
+    Engine.IO client.
+    """
+    VERSION = 3
+    DEFAULT_PATH = '/engine.io'
+
+    def __init__(self, url: str, path: str = None):
+        """
+        Create new Engine.IO client.
+
+        :param url: Server URL.
+        :param path: Engine path (default: "/engine.io").
+        """
+        path = path or self.DEFAULT_PATH
+        self.url = url
+        self.path = path
+        self.session: Optional[aiohttp.ClientSession] = None
+        self.socket: Optional[aiohttp.ClientWebSocketResponse] = None
+        self.runner: Optional[asyncio.Task] = None
+
+        self.ping_interval = 0
+        self.ping_timeout = 0
+        self.last_ping = 0
+
+    @property
+    def connected(self) -> bool:
+        """Are we connected to the server?"""
+        return self.socket is not None
+
+    @property
+    def connection_url(self) -> str:
+        """Full connection URL"""
+        params = {"EIO": self.VERSION, "transport": "websocket"}
+        return urljoin(self.url, f'{self.path}/?{urlencode(params)}')
+
+    async def connect(self):
+        """Connect to server."""
+        if self.connected:
+            return
+
+        self.session = aiohttp.ClientSession()
+        try:
+            logger.debug('Connecting to %s', self.connection_url)
+            self.socket = await self.session.ws_connect(self.connection_url)
+
+            packet = EngineIOPacket.parse(await self.socket.receive_str())
+            if packet.type is not EngineIOPacket.Type.OPEN:
+                raise RuntimeError('Unexpected packet: %s' % packet)
+
+            await self._on_packet(packet)
+
+        except:
+            await self.session.close()
+            self.session = None
+            raise
+
+        self.runner = asyncio.create_task(self._run())
+
+    async def close(self):
+        """Close connection to server."""
+        if self.runner:
+            self.runner.cancel()
+            with contextlib.suppress(asyncio.CancelledError):
+                await self.runner
+
+        if self.socket:
+            await self.socket.close()
+
+        if self.session:
+            await self.session.close()
+
+        self.socket = None
+        self.session = None
+        self.runner = None
+
+    async def _run(self):
+        """Internal runner for consuming messages."""
+        logger.debug('Runner starting')
+        try:
+            while True:
+                now = time.monotonic()
+                if self.ping_interval:
+                    if now > self.last_ping + self.ping_interval:
+                        await self.ping()
+
+                    timeout = self.ping_interval - (now - self.last_ping)
+                else:
+                    timeout = None
+
+                try:
+                    msg = await self.socket.receive(timeout)
+                except asyncio.TimeoutError:
+                    continue
+
+                if msg.type in (aiohttp.WSMsgType.CLOSE, aiohttp.WSMsgType.CLOSED):
+                    break
+                elif msg.type != aiohttp.WSMsgType.TEXT:
+                    logger.warning('Unexpected WebSocket message type: %s', msg.type)
+                else:
+                    try:
+                        packet = EngineIOPacket.parse(msg.data)
+                    except ValueError as e:
+                        logger.warning('Failed to parse packet: %s', e)
+                        continue
+
+                    await self._on_packet(packet)
+        except Exception:
+            logger.exception('Unhandled exception in runner')
+        finally:
+            logger.debug('Runner stopped')
+
+    async def _on_packet(self, packet: EngineIOPacket):
+        """
+        Handle Engine.IO packet.
+
+        :param packet: Decoded packet.
+        """
+        logger.debug('> %s %s', packet.type.name, packet.data)
+        if packet.type is EngineIOPacket.Type.NOOP:
+            pass
+        elif packet.type is EngineIOPacket.Type.OPEN:
+            handshake = json.loads(packet.data)
+            self.sid = handshake['sid']
+            self.ping_interval = handshake['pingInterval'] / SECOND_MS
+            self.ping_timeout = handshake['pingTimeout'] / SECOND_MS
+        elif packet.type is EngineIOPacket.Type.CLOSE:
+            self.sid = None
+            self.ping_interval = None
+            self.ping_timeout = None
+        elif packet.type is EngineIOPacket.Type.PING:
+            await self.send_packet(EngineIOPacket(EngineIOPacket.Type.PONG))
+        elif packet.type is EngineIOPacket.Type.PONG:
+            # TODO: Handle ping timeout
+            pass
+        elif packet.type is EngineIOPacket.Type.MESSAGE:
+            self.on_message(packet.data)
+        elif packet.type is EngineIOPacket.Type.UPGRADE:
+            self.on_upgrade(packet.data)
+        else:
+            logger.warning('Unhandled packet type: %s', packet.type.name)
+
+    @staticmethod
+    def on_message(data: str):
+        """
+        Called when a MESSAGE is received.
+
+        :param data: Message data.
+        """
+        pass
+
+    @staticmethod
+    def on_upgrade(data: str):
+        """
+        Called when an UPGRADE packet is received.
+
+        :param data: Packet data.
+        """
+        pass
+
+    async def ping(self):
+        """Send PING to server."""
+        await self.send_packet(EngineIOPacket(EngineIOPacket.Type.PING))
+        self.last_ping = time.monotonic()
+
+    async def send_message(self, data: str):
+        """
+        Send MESSAGE to server.
+
+        :param data: Message data.
+        """
+        await self.send_packet(EngineIOPacket(EngineIOPacket.Type.MESSAGE, data))
+
+    async def send_packet(self, packet: EngineIOPacket):
+        """
+        Send low-level Engine.IO packet to server.
+
+        :param packet: Engine.IO packet.
+        """
+        if not self.connected:
+            raise RuntimeError('Not connected')
+
+        logger.debug('< %s', packet)
+        await self.socket.send_str(packet.encode())
---
+++ python/adventureland/socketio.py
@@ -0,0 +1,248 @@
+import asyncio
+import collections
+import functools
+import json
+import logging
+import re
+from dataclasses import dataclass
+from enum import Enum
+from typing import *
+
+from adventureland import engineio
+
+JSON = Union[None, int, float, str, bool, List, Dict]
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class SocketIOPacket:
+    """
+    See https://github.com/socketio/socket.io-protocol/tree/v3.
+    """
+    class Type(Enum):
+        CONNECT = 0
+        DISCONNECT = 1
+        EVENT = 2
+        ACK = 3
+        ERROR = 4
+        BINARY_EVENT = 5
+        BINARY_ACK = 6
+
+    type: Type
+    data: Optional[List[JSON]] = None
+    id: Optional[int] = None
+    namespace: str = '/'
+    n_attachments: int = 0
+
+    PACKET_RE = re.compile(
+        r'^(?P<type>\d)(?:(?P<nbin>\d)-)?(?:(?P<nsp>/[^,]*),)?(?P<id>\d+)?(?P<data>.+)?$',
+        re.DOTALL)
+
+    @classmethod
+    def parse(cls, data: str) -> 'SocketIOPacket':
+        """
+        Parse packet data.
+
+        :param data: Raw packet data.
+        """
+        m = cls.PACKET_RE.match(data)
+        if not m:
+            raise ValueError('Malformed packet')
+
+        msg_type = SocketIOPacket.Type(int(m['type']))
+        data = json.loads(m['data']) if m['data'] is not None else None
+        msg_id = int(m['id']) if m['id'] is not None else None
+        namespace = m['nsp'] or '/'
+        n_attachments = int(m['nbin']) if m['nbin'] else 0
+
+        return cls(msg_type, data, msg_id, namespace, n_attachments)
+
+    def encode(self) -> str:
+        """
+        Encode packet.
+        """
+        fields = [str(self.type.value)]
+        if self.data:
+            fields.append(json.dumps(self.data))
+
+        return ''.join(fields)
+
+    def __str__(self):
+        return f'{self.type.name} {self.data}' if self.data else self.type.name
+
+
+class SocketIO:
+    """
+    Socket.IO client.
+    """
+    VERSION = 3
+    DEFAULT_PATH = '/socket.io'
+
+    def __init__(self, url: str, path: str = None):
+        """
+        Create new Socket.IO client.
+
+        :param url: Server URL.
+        :param path: Socket path (default: "/socket.io").
+        """
+        path = path or self.DEFAULT_PATH
+        self.engine = engineio.EngineIO(url, path)
+        self.engine.on_message = self._feed_packet
+        self._handlers = collections.defaultdict(set)
+        self._loop = asyncio.get_event_loop()
+
+    def _feed_packet(self, data: str):
+        """
+        Feed packet data to the client.
+
+        :param data: Packet data.
+        """
+        try:
+            packet = SocketIOPacket.parse(data)
+        except ValueError as e:
+            logger.warning('Failed to parse packet: %s', e)
+            return
+
+        logger.debug('> %s', packet)
+        if packet.type is SocketIOPacket.Type.CONNECT:
+            self.dispatch_event('connect')
+        elif packet.type is SocketIOPacket.Type.DISCONNECT:
+            self.dispatch_event('disconnect')
+        elif packet.type is SocketIOPacket.Type.EVENT:
+            event_name, *event_data = packet.data
+            self.dispatch_event(event_name, *event_data)
+        elif packet.type is SocketIOPacket.Type.ACK:
+            logger.warning('Acknowledgement not implemented')
+        elif packet.type is SocketIOPacket.Type.ERROR:
+            self.dispatch_event('error', packet.data)
+            logger.error('Error: %s', packet.data)
+        elif packet.type is SocketIOPacket.Type.BINARY_EVENT:
+            logger.warning('Binary attachments not implemented')
+        elif packet.type is SocketIOPacket.Type.BINARY_ACK:
+            logger.warning('Binary attachments not implemented')
+        else:
+            logger.warning('Unknown packet type: %s', packet.type)
+
+    def dispatch_event(self, event_name: str, *args: JSON):
+        """
+        Dispatch an event.
+
+        :param event_name: Name of event.
+        :param args: Event arguments.
+        """
+        if event_name not in self._handlers:
+            return
+
+        # make a copy of the handlers, since they may change during iteration
+        for on_event in list(self._handlers[event_name]):
+            try:
+                on_event(*args)
+            except Exception:
+                logger.exception('Exception in event callback')
+
+    def add_event_listener(self, event_name: str, on_event: Callable, once=False):
+        """
+        Register event handler.
+
+        :param event_name: Name of event to listen for.
+        :param on_event: New event listener.
+        :param once: If true, this event will only fire once.
+        """
+        if once:
+            def on_event_once(*args):
+                try:
+                    on_event(*args)
+                finally:
+                    self.remove_event_listener(event_name, on_event_once)
+
+            self._handlers[event_name].add(on_event_once)
+        else:
+            self._handlers[event_name].add(on_event)
+
+    def remove_event_listener(self, event_name: str, on_event: Callable):
+        """
+        Remove event listener.
+
+        :param event_name: Name of event listened for.
+        :param on_event: Previously registered event listener.
+        """
+        self._handlers[event_name].remove(on_event)
+
+    def on(self, event_name: str) -> Callable:
+        """
+        Decorator for registering an event listener.
+
+        :param event_name: Name of event to listen for.
+        """
+        return functools.partial(self.add_event_listener, event_name)
+
+    def once(self, event_name: str) -> Callable:
+        """
+        Decorator for registering a single-shot event listener.
+
+        :param event_name: Name of event to listen for.
+        """
+        return functools.partial(self.add_event_listener, event_name, once=True)
+
+    async def next_event(self, event_name: str = None) -> List[JSON]:
+        """
+        Wait for the next event.
+
+        :param event_name: Name of event.
+        :return: Event data.
+        """
+        future = self._loop.create_future()
+
+        @self.once(event_name)
+        def on_event(*args):
+            future.set_result(list(args))
+
+        return await future
+
+    async def connect(self):
+        """
+        Connect to server.
+        """
+        if not self.engine.connected:
+            await self.engine.connect()
+
+        await self.send_packet(SocketIOPacket(SocketIOPacket.Type.CONNECT))
+        await self.next_event('connect')
+
+    async def close(self):
+        """
+        Close connection.
+        """
+        await self.engine.close()
+
+    async def emit(self, event_name: str, *args: JSON):
+        """
+        Emit an event.
+
+        :param event_name: Name of event.
+        :param args: Event arguments.
+        """
+        await self.send_packet(SocketIOPacket(SocketIOPacket.Type.EVENT, [event_name, *args]))
+
+    async def send_packet(self, packet: SocketIOPacket):
+        """
+        Send low-level Socket.IO packet to server.
+
+        :param packet: Socket.IO packet.
+        """
+        logger.debug('< %s', packet)
+        await self.engine.send_message(packet.encode())
+
+
+async def connect(url: str, **options) -> SocketIO:
+    """
+    Connect to SocketIO server.
+
+    :param url: Server URL.
+    :param options: Additional options.
+    :return: New SocketIO client.
+    """
+    sock = SocketIO(url, **options)
+    await sock.connect()
+    return sock
